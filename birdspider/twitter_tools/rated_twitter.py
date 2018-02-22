ea229cff13d4b901ff360b8818de95eb02195ea7
Giles Richard Greenway <augeas@gmail.com>
2018-02-22 10:23:46 +0000

    RatedTwitter object seems to work. 	modified:   birdspider/twitter_tools/rated_twitter.py

--- birdspider/twitter_tools/rated_twitter.py
+++ birdspider/twitter_tools/rated_twitter.py
@@ -10,7 +10,6 @@ from twitter_settings import *
 
 __twitter_methods__ = filter(lambda m: not m.startswith('__'),dir(EndpointsMixin))
 
-
 class RatedTwitter(object):    
     """Wrapper around the Twython class that tracks whether API calls are rate-limited."""
 
@@ -22,32 +21,38 @@ class RatedTwitter(object):
             self.twitter = Twython(CONSUMER_KEY,access_token=ACCESS_TOKEN)
             self.handle = 'app_'
             
-    def __can_we_do_that__(self,methodName):
+    def can_we_do_that(self,method_name):
         """Check whether a given API call is rate-limited, return the estimated time to wait in seconds.
     
         Positional arguments:
-        methodName -- the name of the API call to test    
+        method_name -- the name of the API call to test    
         """      
-        keyval = cache.get(self.handle+methodName) # Have we recorded how many calls remain in the current window?
-        if not keyval: # We've not made the call for these credentials. Assume all's well.
+        
+        # Have we recorded how many calls remain in the current window?
+        keyval = cache.get(self.handle+method_name)
+        # We've not made the call for these credentials. Assume all's well.
+        if not keyval: 
             return 0
         else:
             history = json.loads(keyval)
-            if history['limit'] > 0: # Still good to go.
+            if history['limit'] > 0:
+                # Still good to go.
                 return 0
-            reset = datetime.strptime( history['reset'].split('.')[0], "%Y-%m-%dT%H:%M:%S" )
+            reset = datetime.strptime(history['reset'].split('.')[0], "%Y-%m-%dT%H:%M:%S")
             rightNow = datetime.now()
-            if reset > rightNow: # No calls left and the window reset is in the future...
-                return (reset - rightNow).seconds + 30 # ...return the time to wait.
+            # No calls left and the window reset is in the future...
+            if reset > rightNow:
+                # ...return the time to wait.
+                return (reset - rightNow).seconds + 30
             return 0
 
-    def __method_call__(self, method_name, *args, **kwargs):
+    def method_call(self, method_name, *args, **kwargs):
         """Make a Twitter API call via the underlying Twython object.
     
         Returns a tuple: (True,<API call return value>) | (False,<reason for failure>)
     
         Positional arguments:
-        methodName -- the name of the API call to test
+        method_name -- the name of the API call to test
 
         """ 
         
@@ -76,7 +81,8 @@ class RatedTwitter(object):
                 result = (False,'unknown')
             print args
 
-        try: # Have we been told how many calls remain in the current window?
+        # Have we been told how many calls remain in the current window?
+        try: 
             xLimit = self.twitter.get_lastfunction_header('x-rate-limit-remaining')
             xReset = self.twitter.get_lastfunction_header('x-rate-limit-reset')
         except:
@@ -86,30 +92,34 @@ class RatedTwitter(object):
             limit = int(xLimit)        
         if xReset:
             reset = datetime.utcfromtimestamp(int(xReset)).isoformat()
-        if xLimit and xReset: # Store the current number of remaining calls and time when the window resets.
+        if xLimit and xReset:
+            # Store the current number of remaining calls and time when the window resets.
             cache.set(self.handle+method_name,json.dumps({'limit':limit, 'reset':reset})) 
 
         return result
-
-
                 
 """
-Attach the following API calls to the ratedTwitter class, so that <ratedTwitter>.<method>(**args) makes the call via <ratedTwitter.__method_call__
-and <ratedTwitter>.<method>_limited() makes the appropriate call to __can_we_do_that__.
+Attach the following API calls to the ratedTwitter class, so that <ratedTwitter>.<method>(*args, **kargs)
+makes the call via <ratedTwitter.method_call and <ratedTwitter>.<method>_wait() makes the appropriate
+call to can_we_do_that.
 """
 
 def method_factory(name):
     def f(self, *args ,**kwargs):
-        return self.__method_call__(name, *args, **kwargs)
+        return self.method_call(name, *args, **kwargs)
     return f
 
 for name in __twitter_methods__:
-    setattr(ratedTwitter,name,method_factory(name))
-    
-    def g(self,name=name):
-        return self.__can_we_do_that__(name)
-    setattr(ratedTwitter,name+'_limited',g)    
-            
+    setattr(RatedTwitter, name, method_factory(name))
+
+def rated_method_factory(name):   
+    def g(self):
+        return self.can_we_do_that(name)
+    return g
+
+for name in __twitter_methods__:
+    setattr(RatedTwitter, name+'_wait', rated_method_factory(name))
+                
 def getTwitterAPI(credentials=False):
     """Return a Twitter API object from oauth credentials, defaulting to those in db_settings."""
     if not credentials:
