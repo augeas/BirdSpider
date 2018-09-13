9559b702a63e658112cbee6b7a1d172a64995426
Giles Richard Greenway <augeas@gmail.com>
2018-09-13 02:41:55 +0100

    modified:   birdspider/solr_tools.py

--- birdspider/db_settings.py
+++ birdspider/db_settings.py
@@ -9,4 +9,8 @@ cache = redis.StrictRedis(host='redis')
 uri = "bolt://neo4j:7687"
 neoDb = GraphDatabase.driver(uri, auth=(environ['NEO_USER'], environ['NEO_PW']))
 
-solrURL = 'http://solr:8983/solr/'
+solr_host = "birdspider_solr"
+
+solr_core = "birdspider"
+
+solrURL = "http://{}:8983/solr/{}".format(solr_host, solr_core)
--- birdspider/solr_tools.py
+++ birdspider/solr_tools.py
@@ -5,32 +5,17 @@ import requests
 from datetime import datetime
 from db_settings import solrURL
 
-solrFields = {
-    'doc_type': {'type':'string','stored':True},
-    'tweet_text' : {'type':'text_en','stored':True, 'termVectors':True, 'termPositions':True, 'termOffsets':True},
-    'tweet_time' : {'type':'date','stored':True}
-}    
 
-#solrDateFields = [ field if solrFields[field]['type'] == 'date' for field in solrFields.keys() ]
-
-def addSolrFields():
-    for fieldName in list(solrFields.keys()):
-        resp = requests.put(solrURL+'schema/fields/'+fieldName,json.dumps(solrFields[fieldName]))
-        if resp.status_code == 200:
-            print('Added field: '+fieldName)
-        else:
-            print("Couldn't add field: '"+fieldName)
-
-
-def addSolrDocs(docs):
-    resp = requests.post(solrURL+'update/json?commit=true',data=json.dumps(docs),headers = {'content-type': 'application/json'})
-    if resp.status_code != 200:
-        print("*** Can't push Solr docs... ***")
+update_url = '/'.join([solrURL,"update/json/docs?commitWithin=1000"])
 
 
 def tweets2Solr(tweets):
     started = datetime.now()
-    addSolrDocs([ {'doc_type':'tweet', 'id':tw['id_str'], 'tweet_text':tw['text'],  'tweet_time':tw['isotime']+'Z'} for tw in tweets ])
+    json_dump = '\n'.join(map(json.dumps, tweets))
+    resp = requests.post(update_url, headers={"Content-Type":"application/json"}, data=json_dump)
     howLong = (datetime.now() - started).seconds
-    print('*** PUSHED '+str(len(tweets))+' TWEETS TO SOLR IN '+str(howLong)+'s ***')
+    if resp.status_code != 200:
+        print("*** Can't push Solr docs... ***")
+    else:
+        print('*** PUSHED '+str(len(tweets))+' TWEETS TO SOLR IN '+str(howLong)+'s ***')
 
